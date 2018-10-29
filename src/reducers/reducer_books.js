f5087a123b8c5dcc85025bcffa2a9e9b9a92f0cc
John <casillaj@oregonstate.edu>
2018-10-29 14:37:02 -0600

    Adding reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'JS: The good parts'},
+        {title: 'Harry Potter'},
+        {title: 'Dark tower'},
+        {title: 'eloquent ruby'}
+    ]
+}
